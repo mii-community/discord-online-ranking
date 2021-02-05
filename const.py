95605b787bddbe2208b60456ae9df6a5838f6f23
tenzyu <tenzyu.on@gmail.com>
2021-02-06 00:53:24 +0900

    setting

--- const.py
+++ const.py
@@ -8,5 +8,6 @@ load_dotenv()
 DISCORD_BOT_TOKEN = getenv("DISCORD_BOT_TOKEN")
 
 # setting
-BOT_NAME = "BOT TEMPLATE"
-BOT_PREFIX = "?"
+BOT_NAME = "MII ONLINE RANKING"
+BOT_PREFIX = "!"
+MONGO_URL = str(getenv("MONGO_URL"))
